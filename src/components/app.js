b99da7f1745bc754d9e90dc26bd809f02c951e9e
Mac1 <bischi8@gmail.com>
2020-06-15 09:56:25 -0700

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Rob Toutons Portfolio</h1>
+        <h2>Cell Phone: (408) 306-8466</h2>
+        <h2>Email: bischi8@gmail.com</h2>
+        <h2>GitHub: jrt509</h2>
+       
+      </div>
+    );
+  }
+}
