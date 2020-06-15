46aa4c3727e8f894fe6931e9daae1ac4c1a3533b
Mac1 <bischi8@gmail.com>
2020-06-15 13:40:41 -0700

    fixed: updated name of project

--- src/components/app.js
+++ src/components/app.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import moment from "moment";
 
 export default class App extends Component {
   render() {
@@ -8,7 +9,7 @@ export default class App extends Component {
         <h2>Cell Phone: (408) 306-8466</h2>
         <h2>Email: bischi8@gmail.com</h2>
         <h2>GitHub: jrt509</h2>
-       
+      <div>{ moment().format("MMMM Do YYYY, h:mm:ss a") }</div>
       </div>
     );
   }
