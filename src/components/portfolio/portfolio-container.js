b00842324a86acde4435c4e6a19f396d6a724ec4
Mac1 <bischi8@gmail.com>
2020-06-17 13:56:27 -0700

    added navigation

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -5,16 +5,24 @@ export default class PortfolioContainer extends Component {
     constructor() {
         super();
 
-        this.state= {
+        this.state = {
             pageTitle: "Welcome to my portfolio",
+            isLoading: false,
             data: [
 
-         {title: "Devcamp Fries"},
-         {title: "Wild Cherry Slots"}, 
-         {title: "Capstone Project"}
+         {title: "Devcamp Fries", category: "Ecommerce"},
+         {title: "Wild Cherry Slots", category: "Gaming"}, 
+         {title: "Capstone Project", category: "Learning"}
         ]
         };
-        this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this)
+       this.handleFilter = this.handleFilter.bind(this);
+    }
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        })
     }
 
     portfolioItems() {
@@ -24,18 +32,20 @@ export default class PortfolioContainer extends Component {
             return <PortfolioItems title={item.title} />;
         })
     }
-    handlePageTitleUpdate() {
-        this.setState({
-            pageTitle: "Something Else"
-        });
-    }
+    
+    
     render() {
+        if (this.state.isLoading) {
+            return <div>Loading...</div>;
+        }
         return (
             <div>
                 <h2>{this.state.pageTitle}</h2>
+                <button onClick={() => this.handleFilter('Ecommerce')}>Ecommerce</button>
+                <button onClick={() => this.handleFilter('Gaming')}>Gaming</button>
+                <button onClick={() => this.handleFilter('Learning')}>Learning</button>
                 {this.portfolioItems()}
-                <hr/>
-                <button onClick={this.handlePageTitleUpdate}>Change Title</button>
+               
             </div>
         )
     }
