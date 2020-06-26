e27397c1284e3c7cb8e40735a8012a697cd955a5
Mac1 <bischi8@gmail.com>
2020-06-26 15:16:52 -0700

    created styles and blog posts

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,12 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-export default function() {
-    return (
-        <div>
-            <h2>Blog</h2>
-            <div>
-                <Link to ="/about-me">Blog about me</Link>
+import axios from 'axios';
+import BlogItem from "../blog/blog-item";
+
+
+class Blog extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            blogItems: []
+        };
+        this.getBlogItems = this.getBlogItems.bind(this);
+        this.activateInfiniteScroll();
+    }
+    activateInfiniteScroll() {
+        window.onscroll = () => {
+            if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+                console.log("get more posts");
+            }
+        }
+    }
+    getBlogItems() {
+        axios
+            .get("https://robtouton.devcamp.space/portfolio/portfolio_blogs", {
+                 withCredentials: true 
+                })
+            .then(response => {
+                this.setState({
+                    blogItems: response.data.portfolio_blogs
+                })
+            })
+            .catch(error => {
+                console.log("getBlogItems error", error)
+            });
+        }
+    componentWillMount() {
+        this.getBlogItems();
+    }
+
+
+    render() {
+        const blogRecords = this.state.blogItems.map(blogItem => {
+            return <BlogItem key={blogItem.id} blogItem={blogItem} />
+        });
+        
+        return (
+            <div className="blog-container">
+                <div className="content-container">{blogRecords}</div>
             </div>
-        </div>
-    );
+        )
+        
+    }
 }
+export default Blog;
