ec0b54c47b4bcb457b4f106c4eb0c3d6325228b8
Mac1 <bischi8@gmail.com>
2020-06-29 16:43:17 -0700

    created helper folder to import icons in

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -92,11 +92,14 @@ class Blog extends Component {
                 handleSuccessfulNewBlogSubmission={this.handleSuccessfulNewBlogSubmission}
                 handleModalClose={this.handleModalClose} 
                 modalIsOpen={this.state.blogModalIsOpen} />
+                
+                {this.props.loggedInStatus === "LOGGED_IN" ? (
                 <div className="new-blog-link">
                     <a onClick={this.handleNewBlogClick}>
                         <FontAwesomeIcon icon="plus-circle" />
                     </a>
                 </div>
+                ) : null}
                 <div className="content-container">{blogRecords}</div>
 
                 {this.state.isLoading ? (
