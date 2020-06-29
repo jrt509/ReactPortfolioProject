db29c3d0079362f894f70057df8932ec9d466488
Mac1 <bischi8@gmail.com>
2020-06-29 13:43:49 -0700

    styled modal component

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from 'axios';
 import BlogItem from "../blog/blog-item";
+import BlogModal from "../modals/blog-modal";
 
 
 class Blog extends Component {
@@ -13,11 +14,24 @@ class Blog extends Component {
             blogItems: [],
             totalCount: 0,
             currentPage: 0,
-            isLoading: true
+            isLoading: true,
+            blogModalIsOpen: false
         };
         this.getBlogItems = this.getBlogItems.bind(this);
         this.onScroll = this.onScroll.bind(this);
         window.addEventListener("scroll", this.onScroll, false);
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
+    }
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        })
+    }
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true
+        });
     }
     onScroll() {
             if (this.state.isLoading || 
@@ -67,6 +81,12 @@ class Blog extends Component {
         
         return (
             <div className="blog-container">
+                <BlogModal
+                handleModalClose={this.handleModalClose} 
+                modalIsOpen={this.state.blogModalIsOpen} />
+                <div className="new-blog-link">
+                    <a onClick={this.handleNewBlogClick}>Open Modal</a>
+                </div>
                 <div className="content-container">{blogRecords}</div>
 
                 {this.state.isLoading ? (
