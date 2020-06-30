b567c8900091ff0c2fa3f94d708fe1d1cdb2e4c6
Mac1 <bischi8@gmail.com>
2020-06-30 16:52:11 -0700

    fixed bug with modal styles

--- src/components/forms/rich-text-editor.js
+++ src/components/forms/rich-text-editor.js
@@ -12,7 +12,9 @@ export default class RichTextEditor extends Component {
        this.state = {
            editorState: EditorState.createEmpty()
        };
-       this.onEditorStateChange = this.onEditorStateChange.bind(this)
+       this.onEditorStateChange = this.onEditorStateChange.bind(this);
+       this.getBase64 = this.getBase64.bind(this);
+       this.uploadFile = this.uploadFile.bind(this);
     }
     onEditorStateChange(editorState) {
         this.setState(
@@ -23,6 +25,18 @@ export default class RichTextEditor extends Component {
         );
         
     }
+    getBase64(file, callback) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => callback(reader.result);
+        reader.onerror = error => {};
+        
+    }
+    uploadFile(file) {
+        return new Promise((resolve, reject) => {
+            this.getBase64(file, data => resolve({data: { link: data }}))
+        })
+    }
 
     render() {
         return (
@@ -32,6 +46,19 @@ export default class RichTextEditor extends Component {
                 wrapperClassName="demo-wrapper"
                 editorClassName="demo-editor"
                 onEditorStateChange={this.onEditorStateChange}
+                toolbar={{
+                    inline: { inDropdown: true },
+                    list: { inDropdown: true },
+                    textAlign: { inDropdown: true },
+                    link: { inDropdown: true },
+                    history: { inDropdown: true },
+                    image: {
+                        uploadCallback: this.uploadFile,
+                        alt: { present: true, mandatory: false },
+                        previewImage: true,
+                        inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+                    }
+                }}
                 />
             </div>
         )
